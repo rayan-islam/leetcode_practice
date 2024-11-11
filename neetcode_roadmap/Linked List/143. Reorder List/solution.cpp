ce90efce4d94cf1c1445afdaf1f9d3278c1c3b64
token-of-love <rayan.islam.2586@gmail.com>
2024-11-11 20:09:25 +0600

    143. Reorder list

--- neetcode_roadmap/Linked List/143. Reorder List/solution.cpp
+++ neetcode_roadmap/Linked List/143. Reorder List/solution.cpp
@@ -11,6 +11,44 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        if(!head || !head->next || !head->next->next){
+            return;
+        }
         
+        ListNode* fast=head;
+        ListNode* slow=head;
+        
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        ListNode* prev=nullptr;
+        ListNode* temp=slow->next;
+        slow->next=nullptr;
+        slow = temp;
+        while(slow){
+            temp=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=temp;
+        }
+        
+        ListNode* ans=new ListNode();
+        while(head ||prev){
+            if(head){
+                ans->next=head;
+                ans=ans->next;
+                head=head->next;
+            }
+            if(prev){
+
+                ans->next=prev;
+                ans=ans->next;
+                prev=prev->next;            
+            }
+        }
+        head=ans->next;
+
     }
 };
